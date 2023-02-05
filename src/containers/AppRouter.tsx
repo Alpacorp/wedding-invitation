2f6d81293a9387d724b51086273b9db915121ee9
Alpacorp <alejandro.palacios88@gmail.com>
2023-02-04 23:23:00 -0500

    create container component

--- src/containers/AppRouter.tsx
+++ src/containers/AppRouter.tsx
@@ -1,13 +1,14 @@
+import { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
-import { Home } from "../Pages/Home";
+import DataContext from "../context/dataContext";
+import { Home } from "../pages/home";
+import { Splash } from "../pages/splash";
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/?utm_source" element={<Home />} />
-      <Route path="/:username" element={<Home />} />
       <Route path="/*" element={<Home />} />
+      <Route path="/" element={<Splash />} />
     </Routes>
   );
 };
