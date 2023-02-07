04cac3f3332de19d269000cc05b679766385a918
Alpacorp <alejandro.palacios88@gmail.com>
2023-02-06 22:26:02 -0500

    Add Home Page into Router DOM

--- src/containers/AppRouter.tsx
+++ src/containers/AppRouter.tsx
@@ -2,12 +2,15 @@ import { Route, Routes } from "react-router-dom";
 
 import { HomePage } from "../pages/home";
 import { IntroPage } from "../pages/intro";
+import { Admin } from "../pages/admin";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/*" element={<HomePage />} />
       <Route path="/" element={<IntroPage />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/invitacion" element={<HomePage />} />
     </Routes>
   );
 };
