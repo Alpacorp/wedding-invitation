573444ab2f81eb0a130729848cf3c8417339e333
Alpacorp <alejandro.palacios88@gmail.com>
2023-02-08 00:14:43 -0500

    Change max width properties

--- src/components/VideoIntro/VideoIntro.tsx
+++ src/components/VideoIntro/VideoIntro.tsx
@@ -5,7 +5,7 @@ export const VideoIntro = () => {
     <section className="video">
       <iframe
         width="100%"
-        height="215"
+        height="200"
         src="https://www.youtube.com/embed/Zw_IigNMVkY"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
