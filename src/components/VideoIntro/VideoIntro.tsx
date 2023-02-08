b441f62d7b68a5b1f5dd415ec2f1e0b5db720905
Alpacorp <alejandro.palacios88@gmail.com>
2023-02-08 11:41:10 -0500

    Fix width video layout

--- src/components/VideoIntro/VideoIntro.tsx
+++ src/components/VideoIntro/VideoIntro.tsx
@@ -4,7 +4,6 @@ export const VideoIntro = () => {
   return (
     <section className="video">
       <iframe
-        width="100%"
         height="200"
         src="https://www.youtube.com/embed/Zw_IigNMVkY"
         title="YouTube video player"
